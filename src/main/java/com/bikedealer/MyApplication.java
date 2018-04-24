291b66b3ee81fa0c511cc238c8d8d7663f6b5404
PawelJa <janecki.paw@gmail.com>
2018-04-24 18:55:16 +0200

    main class

---
+++ src/main/java/com/bikedealer/MyApplication.java
@@ -0,0 +1,24 @@
+package com.bikedealer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+public class MyApplication {
+
+	@Bean(name = "messageSource")
+	public ReloadableResourceBundleMessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageBundle = new ReloadableResourceBundleMessageSource();
+		messageBundle.setBasename("classpath:messages/messages");
+		messageBundle.setDefaultEncoding("UTF-8");
+		return messageBundle;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApplication.class, args);
+
+    }       
+}            
